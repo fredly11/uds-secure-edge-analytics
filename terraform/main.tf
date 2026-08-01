c384938bca7a9b1d535bcc7d3c27561f7b1a0150
fredly11 <buechelewd@gmail.com>
2026-08-01 02:35:10 +0000

    Added UDS bundle

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ module "eks" {
   version = "~> 20.0"  
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.30"
+  cluster_version = "1.32"
 
   cluster_endpoint_public_access = true
   cluster_endpoint_private_access = true
